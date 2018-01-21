ae86b65ef434518e6dd98a8f2a7656f7f3275a4d
sgritz <stephgritz@gmail.com>
2018-01-20 20:25:37 -0800

    Activity 2

---
+++ Week3/Act2Script.js
@@ -0,0 +1,10 @@
+square(4);
+
+function square(input){
+	console.log("The square of " + input + ": " + input*input)
+}
+
+cube(4);
+var cube = function(input){
+	console.log("The cube of " + input + ": " + input*input*input)
+}
