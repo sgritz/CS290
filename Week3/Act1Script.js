70c11c6852971ff4cea41bfbeae5b8fe8e03b73c
sgritz <stephgritz@gmail.com>
2018-01-20 20:26:36 -0800

    Print tweak
    Message printed tweak

--- Week3/Act1Script.js
+++ Week3/Act1Script.js
@@ -1,5 +1,5 @@
-console.log("Lets make sure JavaScript is working.");
 var name = "Stephanie"; //Replace this with your first name
+console.log("Lets make sure JavaScript is working, " + name + ".");
 console.log("The unicode characters of your name are:")
 for (var i = 0; i < name.length; i++){
 	console.log(name.charCodeAt(i));
